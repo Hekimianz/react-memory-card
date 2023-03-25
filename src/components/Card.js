2eff487dc79194ddf70969f5c3e36cfa116780b1
Aram Hekimian <aramhek121@gmail.com>
2023-03-24 20:35:24 -0600

    cards shuffle when clicked on and style

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,25 @@ import "./Card.css";
 export default function Card(props) {
   const str = props.data.name;
   const str2 = str.charAt(0).toUpperCase() + str.slice(1);
+  const [styles, setStyles] = React.useState({
+    transition: ".3s all",
+    transform: "scale(0)",
+  });
+  React.useEffect(() => {
+    setStyles({
+      transition: ".3s all",
+      transform: "scale(1) rotate(360deg)",
+    });
+  }, []);
+
   return (
-    <div className="card--cont">
-      <img className="card--img" src={props.data.sprites.front_default} />
+    <div onClick={props.handleClick} className="card--cont">
+      <img
+        className="card--img"
+        src={props.data.sprites.front_default}
+        alt=""
+        style={styles}
+      />
       <h1 className="card--name">{str2}</h1>
     </div>
   );
--- src/components/Game.js
+++ src/components/Game.js
@@ -4,9 +4,15 @@ import { nanoid } from "nanoid";
 import "./Game.css";
 export default function Game(props) {
   const [results, setResults] = React.useState([]);
-  const level = 3;
+
+  const level = 5;
   function shuffle(array) {
-    return array.sort(() => Math.random() - 0.5);
+    const arr = array.sort(() => Math.random() - 0.5);
+    const arr2 = arr.sort(() => Math.random() - 0.5);
+    const arr3 = arr2.sort(() => Math.random() - 0.5);
+    const arr4 = arr3.sort(() => Math.random() - 0.5);
+    const arr5 = arr4.sort(() => Math.random() - 0.5);
+    return arr5;
   }
   const shuffled = shuffle(props.data);
   React.useEffect(() => {
@@ -21,8 +27,29 @@ export default function Game(props) {
     });
   }, [props.data]);
 
-  const cards = results.map((item) => {
-    return <Card key={nanoid()} data={item} />;
+  function checkShuffle(arr1, arr2) {
+    if (arr1[0].name === arr2[0].name) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function shuffleOnClick() {
+    setResults((prevData) => {
+      const arr = [...prevData];
+      const shuffledArr = shuffle(arr);
+      if (checkShuffle(prevData, shuffledArr) === true) {
+        return shuffledArr;
+      } else {
+        return shuffle(shuffledArr);
+      }
+    });
+  }
+
+  let cards = results.map((item) => {
+    return <Card handleClick={shuffleOnClick} key={nanoid()} data={item} />;
   });
+
   return <div className="game--cont">{cards}</div>;
 }
